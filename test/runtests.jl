c7908c14cf33fe424bb35f28a6dd2108bac327d1
anton083 <anton.sollman@outlook.com>
2023-10-26 01:18:20 +0200

    Added tests for 1ASS

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,6 @@ using AssigningSecondaryStructure
 using Test
 
 @testset "AssigningSecondaryStructure.jl" begin
-    out = dssp("1ZAK.pdb")
-    @test [count(==(1), out) count(==(2), out) count(==(3), out)] == [144 232 64]
+    out = dssp("1ASS.pdb")
+    @test [count(==(1), out) count(==(2), out) count(==(3), out)] == [68 53 39]
 end
