608c10b8cec6fd54e306205225391a36674df65b
anton083 <anton.sollman@outlook.com>
2023-10-26 02:57:17 +0200

    Tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using AssigningSecondaryStructure
 using Test
 
 @testset "AssigningSecondaryStructure.jl" begin
-    out = dssp("1ASS.pdb")
-    @test [count(==(1), out) count(==(2), out) count(==(3), out)] == [68 53 39]
+    @test ss_composition.(dssp("data/1ASS.pdb")) == [[60 53 39]]
+    @test ss_composition.(dssp("data/1ZAK.pdb")) == [[72 116 32], [72 116 32]]
+    @test ss_composition.(dssp("data/3GOU.pdb")) == [[40 101 0], [44 102 0], [40 101 0], [44 102 0]]
 end
