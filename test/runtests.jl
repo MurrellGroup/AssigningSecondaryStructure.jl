cf67c59b3f014fd535ca475814745b97e3dc3ead
anton083 <anton.oresten42@gmail.com>
2024-09-01 23:47:30 +0200

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,31 +2,20 @@ using AssigningSecondaryStructure
 using Test
 
 import AssigningSecondaryStructure as ASS
-import Backboner
+
+using BioStructures
 
 ss_composition(secondary_structure::Vector{Int}) = [count(==(ss), secondary_structure) for ss in 1:3]
 
 @testset "AssigningSecondaryStructure.jl" begin
 
-    @testset "DSSP" begin
+    backbones = map(collectchains(read("data/1ZAK.pdb", PDBFormat))) do chain
+        reshape(coordarray(chain, backboneselector), 3, 4, :)[:, 1:3, :]
+    end
 
-        @testset "dssp" begin
-            coords = reshape(Backboner.Protein.readpdb("data/1ASS.pdb")[1].backbone.coords, 3, 3, :)
-            @test AssigningSecondaryStructure.dssp(coords[:, :, 35:39]) == [1, 1, 1, 1, 1] # minimum helix length is 4
-            @test AssigningSecondaryStructure.dssp(coords[:, :, 35:40]) == [1, 2, 2, 2, 2, 1]
-            @test AssigningSecondaryStructure.dssp(coords[:, :, 35:41]) == [1, 2, 2, 2, 2, 2, 1]
-        end
-
-        @testset "1ASS" begin
-            ss = assign_secondary_structure("data/1ASS.pdb")
-            @test ss_composition.(ss) == [[60, 53, 39]]
-        end
-
-        @testset "1ZAK" begin
-            ss = assign_secondary_structure("data/1ZAK.pdb")
-            @test ss_composition.(ss) == [[72, 116, 32], [72, 116, 32]]
-        end
-
+    @testset "1ZAK" begin
+        ss = assign_secondary_structure(backbones)
+        @test ss_composition.(ss) == [[72, 116, 32], [72, 116, 32]]
     end
 
 end
